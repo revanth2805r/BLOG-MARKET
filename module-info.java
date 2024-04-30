7e23b6ab7e08c5a69e01a83396d08821be1fc4be
revanth2805r <123436669+revanth2805r@users.noreply.github.com>
2024-04-30 12:26:25 +0530

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,9 @@
+module Revanth {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.sql;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
